46a52eef99d76b07147cd5c1f296f82b945903f9
George Mencoff <george.mencoff@gmail.com>
2025-04-07 21:27:03 -0400

    update auth state loading

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-    const [user, loading] = useAuthState(auth);
     const router = useRouter();
 
     useEffect(() => {
-        if (!loading) {
-            router.push(user ? "/editor" : "/login");
-        }
-    }, [user, loading, router]);
+        const unsubscribe = auth.onAuthStateChanged(
+            (user) => {
+                router.push(user?.uid ? "/editor" : "/login");
+            }
+        );
+        return unsubscribe;
+    }, [router]);
 
     return <p>Loading...</p>;
 }
